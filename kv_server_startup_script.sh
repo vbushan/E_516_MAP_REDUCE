4da83d976290bd1f98fadd042cc4f9536c74a8f7
Vamsi <vbushan@iu.edu>
2020-10-21 21:33:24 -0400

    Added virtual env activation scripts

--- kv_server_startup_script.sh
+++ kv_server_startup_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd /usr/vbushan/E_516_MAP_REDUCE
+source map_reduce_env/bin/activate
 python3 kv_store.py
--- master_startup_script.sh
+++ master_startup_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd /usr/vbushan/E_516_MAP_REDUCE
+source map_reduce_env/bin/activate
 python3 master.py
